baa9d0ac768ec4f06babe745df878bcde0d15657
Michal Kowalski <michal_kowa@op.pl>
2021-09-16 23:05:06 +0200

    ModyficationsMainContent

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using CardManager.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+
+            return View(registerViewModel);
+        }
+    }
+}
